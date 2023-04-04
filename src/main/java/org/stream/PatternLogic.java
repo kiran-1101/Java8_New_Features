c7a05e9023c90e89cc92944f37b341593f35258e
Kiran Lendwe <lendwekiran@gmail.com>
2023-04-04 10:17:28 +0530

    added changes for pattern program

--- src/main/java/org/stream/PatternLogic.java
+++ src/main/java/org/stream/PatternLogic.java
@@ -3,22 +3,27 @@ package org.stream;
 public class PatternLogic {
 
     public static void main(String[] args) {
-        for (int i = 1; i<=6; i++)
-        {
+        char[] arrLetters = {'a', 'b', 'c', 'd', 'e', 'f'};
+        for (int i = 1; i <= 6; i++) {
 
-
-            for (int j = 1; j<= i;j++)
-            {
-                if(i%2 != 0)
-                {
-                    System.out.print(i+""+(char)(i+64));
-                }
-                else {
-                    System.out.print(i + "" + (char) (i + 64));
+            String str = "";
+            for (int j = 1; j <= i; j++) {
+                if(i%2==0){
+                    if (j % 2 != 0) {
+                        str = str + arrLetters[i-1];
+                    } else {
+                        str = str + i;
+                    }
+                } else {
+                    if (j % 2 == 0) {
+                        str = str + arrLetters[i-1];
+                } else {
+                        str = str + i;
+                    }
                 }
 
             }
-            System.out.println();
+            System.out.println(str);
         }
     }
 }
